ca5e439d2abcc1006326c9c47ecebc57cde26e4d
David Marsh <40401344+Daveym1@users.noreply.github.com>
2022-12-05 18:56:08 +0000

    Question and answer options are displayed

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,6 +4,13 @@ var questionContainer = document.getElementById("questions")
 var questionDiv = document.getElementById("question-title");
 var question = localStorage.setItem("questions", JSON.stringify(questions));
 var startScreen = document.getElementById("start-screen");
+var choices = document.getElementById("choices");
+var questionIndex = 0;
+var userChoices = questions[questionIndex].Choices;
+
+    var choicesList = document.createElement("ol");
+    var listItems = document.createElement("li");
+
 // Pseudo Code
 
 //Create a code quiz that contains the following requirements:
@@ -14,7 +21,7 @@ var startScreen = document.getElementById("start-screen");
         // 1. When clicked the timer starts.
 
 button.addEventListener("click", function(){
-    timerCount.innerHTML = 10;
+    timerCount.innerHTML = 60;
     var countdown = setInterval(function(){
         timerCount.innerHTML--;
 
@@ -22,22 +29,89 @@ button.addEventListener("click", function(){
             console.log("Time's up");
             clearInterval(countdown);
         }
+    renderQuestions(questionIndex);
     }, 1000)
 
-startScreen.classList.add("hide");
-document.getElementById("question-title").classList.remove("hide");
-
+    startScreen.classList.add("hide");
+    document.getElementById("question-title").classList.remove("hide");
+    questionContainer.classList.remove("hide");
 })
 
-console.log(question);
+
+// 2. The first question is displayed. and #start-screen is hidden
+
+function renderQuestions(questionIndex){
+
+for (i = 0; i < questions.length; i++){
+    questionDiv.textContent = questions[0].Question;
+    var userChoices = questions[questionIndex].Choices;
+
+    var choicesList = document.createElement("ol");
+    
+    
+    // function createListItems() {
+    //     // var listItems = document.createElement("li");
+    //     // var btn = document.createElement("button");
+    //     btn.textContent = userChoices;
+    //     return listItems, btn;
+    // }
+    
+    // Questions contain buttons for each answer.
+    userChoices.forEach(function (newItem) {
+        var listItems = document.createElement("button");
+        listItems.textContent = newItem;
+        questionDiv.appendChild(choicesList);
+        choicesList.appendChild(listItems);
+        listItems.addEventListener("click", (compare));
+
+    });
+}
+}
+
+// function to compare answer with options
+
+function compare(event){
+    var element = event.target;
+
+    if (element.matches("button")) {
+       
+        // Correct condition 
+        if (element.textContent == questions[questionIndex].Answer) {
+            console.log("correct");
+             // Correct condition 
+             questionIndex++;
+        } 
+
+    }
+    }
 
 
-        // 2. The first question is displayed. and #start-screen is hidden
 
 
 
 
-// Questions contain buttons for each answer.
+
+
+
+choices.appendChild(choicesList);
+// choicesList.appendChild(createListItems());
+// choicesList.appendChild(createListItems());
+// choicesList.appendChild(createListItems());
+// choicesList.appendChild(createListItems());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // Below the question will be four buttons containing possible answers
 
@@ -60,4 +134,5 @@ console.log(question);
 
     //A prompt or form will display, showing players score and asking to input their initials. 
     //Their score will then be displayed alongside their initials on the high score page
-    //Will need to put their initials and score in local storage and retrieve it on high score page
+
+//Will need to put their initials and score in local storage and retrieve it on high score page
